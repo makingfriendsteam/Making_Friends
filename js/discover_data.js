cbe3a117c6daf51c70c7b7d9146eaaed5451147f
Jimin Yook <sprkal129@gmail.com>
2019-02-27 22:30:07 -0600

    added match alert

--- js/discover_data.js
+++ js/discover_data.js
@@ -3,19 +3,15 @@ $( document ).ready(function() {
 
 
     prof1=new Profile("Kyle", "Smith", "22", "Chicago, IL", "Hey what's uppppppppp!!!","profiles/Kyle/drake.jpg", false)
-    prof2=new Profile("Steve", "Smith", "20", "Evanston, IL", "Hey what's uppppppppp!!!","profiles/Steve/George-Clooney.jpg", true)
-    prof3=new Profile("Daniel", "Smith", "19", "New York, NY", "Hey what's uppppppppp!!!","profiles/Daniel/guy1.jpg",false)
-    prof4=new Profile("Kevin", "Smith", "32", "Los Angeles, CA", "Hey what's uppppppppp!!!","profiles/Kevin/jackblack.jpg",true)
-    prof5=new Profile("Tim", "Smith", "40", "Chicago, IL", "Hey what's uppppppppp!!!","profiles/Tim/ryan_gosling-2.jpg",false)
+    prof2=new Profile("Steve", "Smith", "20", "Evanston, IL", "Just hanging around.","profiles/Steve/George-Clooney.jpg", true)
+    prof3=new Profile("Daniel", "Smith", "19", "New York, NY", "Who wants to play a game o' ball with me?","profiles/Daniel/guy1.jpg",false)
+    prof4=new Profile("Kevin", "Smith", "32", "Los Angeles, CA", "Looking for a music-lover to join my band!","profiles/Kevin/jackblack.jpg",true)
+    prof5=new Profile("Tim", "Smith", "40", "Chicago, IL", "Introverts only.","profiles/Tim/ryan_gosling-2.jpg",false)
 
     var profiles = [prof1,prof2,prof3,prof4,prof5];
 
-    for (i = 0; i < profiles.length; i++) {
 
-      // $('ul.card-list').append('<li class="card' +
-      //                         i==0? ' current' : '' + '"' +
-      //                         ' id="profile' + i +'">' +
-      //                         profilecard_html);
+    for (i = 0; i < profiles.length; i++) {
 
       currentClass = i==0?'current':''
 
@@ -29,9 +25,33 @@ $( document ).ready(function() {
       $('#profile' + i + ' .discover-profile-location').html(profiles[i].location)
       $('#profile' + i + ' .discover-profile-introduction').html(profiles[i].introduction)
 
-
-
     }
+
+    var currIdx = 0;
+    $('#JPO').popup();
+
+    $("button.but-yay").click(function(){
+      if(profiles[currIdx].likesYou){
+          name=profiles[currIdx].firstName
+          alert("Congratulations. you and " + name + " liked each other!\n\n" + name + " has been added to your List of Matches.")
+          //$('#JPO').popup();
+      }
+    });
+
+    $("button.but-yay, button.but-nope").click(function(){
+      if (currIdx == profiles.length-1){
+        $("ul.card-list").html("No more profiles to show. Try again later!");
+        $("button.but-nope, button.but-yay").prop("disabled",true);
+        $("button.but-nope, button.but-yay").css("background","gray");
+      } else if (currIdx < profiles.length){
+        currIdx++;
+      }
+      console.log(currIdx);
+    })
+
+
+
+
 
 
 });
